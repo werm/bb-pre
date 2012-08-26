f35ffe80d255e394d6b7a6347de2054f0eba68c4
Craig Wermert <craig@wermert.org>
2012-08-26 17:26:55 +0400

    rails_apps_composer: prelaunch app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
-BbPre::Application.routes.draw do
+RailsPrelaunchSignup::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
-  devise_for :users
-  resources :users, :only => [:show, :index]
+  devise_scope :user do
+    root :to => "devise/registrations#new"
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+  end
+  devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
+  match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
+  resources :users, :only => [:show, :index] do
+    get 'invite', :on => :member
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120826132527) do
+ActiveRecord::Schema.define(:version => 20120826132614) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20120826132527) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.boolean  "opt_in"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
